0815f4c4dd077281ebe8bc1db04f6f19f4fa956a
Vaginin <akinin7657@gmail.com>
2024-06-09 21:02:04 +0300

    fix(data-service): add data service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { DataTableComponent } from './data-table/data-table.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: DataTableComponent }
+];
